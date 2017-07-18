d76d50c9d196260e3da64040ff259da385fc9ffc
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-18 14:23:30 +0100

    Added parms

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Ginger", "Kitty", "Simba"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @color = params[:color] || "red"
   erb(:index)
 end
